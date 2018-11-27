5c139f777ea49bfde7ff40862b018b5f9dea1238
nkarthi021 <nkarthi021@gmail.com>
2018-11-27 18:23:32 +0530

    Added Task Manager API Performance Test

--- TaskManager.API/App_Start/WebApiConfig.cs
+++ TaskManager.API/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace TaskManager.API
 {
@@ -19,6 +20,9 @@ namespace TaskManager.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
